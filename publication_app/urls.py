7608d565dc9744fd00a65b4ce0042f90b2a6710a
Danila Zakharchuk <zakharchukdanila@gmail.com>
2022-05-04 21:24:40 +0300

    main music team pages, bg, favicon

--- publication_app/urls.py
+++ publication_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from publication_app.views import main_page, team_page
+from publication_app.views import music_page, team_page, main_page
 
 urlpatterns = [
     path('', main_page, name='main_page'),
-    path('team/', team_page, name='team_page')
+    path('team/', team_page, name='team_page'),
+    path('radio/', music_page, name='music_page')
 ]
--- publication_app/views.py
+++ publication_app/views.py
@@ -7,12 +7,12 @@ from django.shortcuts import render
 from .models import Post, People
 
 
-def main_page(request):
+def music_page(request):
     posts = Post.objects.filter(is_public=True).order_by('-created_at', '-id').all()
     #posts = ({'title': random.randint(100, 1_000_000), 'text': 'nmjisnidmfgsmfosmifsdfjisnfisn'} for _ in range(100))
-    context = {'title': 'HELLO?', 'posts': posts}
+    context = {'title': 'radio', 'posts': posts}
 
-    return render(request, 'main_page.html', context)
+    return render(request, 'music_page.html', context)
 
 
 def team_page(request):
@@ -21,3 +21,9 @@ def team_page(request):
     context = {'title': 'Team', 'team': people}
 
     return render(request, 'team_page.html', context)
+
+
+def main_page(request):
+    context = {'title': 'Main', }
+
+    return render(request, 'main_page.html', context)
