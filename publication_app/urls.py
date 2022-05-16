aff34aa80c57108c2ef30b439f6b9f7529bfeeca
Danila Zakharchuk <zakharchukdanila@gmail.com>
2022-05-16 17:08:15 +0300

    added registration and auth to users

--- publication_app/urls.py
+++ publication_app/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 
-from publication_app.views import music_page, team_page, main_page
+from publication_app.views import music_page, team_page, main_page, registration_page, login_page, logout_page
 
 urlpatterns = [
     path('', main_page, name='main_page'),
     path('team/', team_page, name='team_page'),
-    path('radio/', music_page, name='music_page')
+    path('radio/', music_page, name='music_page'),
+    path('registration/', registration_page, name='reg_page'),
+    path('login/', login_page, name='log_page'),
+    path('logout/', logout_page, name='logout_page')
 ]
--- publication_app/views.py
+++ publication_app/views.py
@@ -1,9 +1,13 @@
 import random
 
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import AuthenticationForm
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from .forms.registration import RegistrationForm
 from .models import Post, People
 
 
@@ -27,3 +31,41 @@ def main_page(request):
     context = {'title': 'Main', }
 
     return render(request, 'main_page.html', context)
+
+
+def registration_page(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {
+        'reg_form': RegistrationForm(),
+    }
+    return render(request, 'registration_page.html', context)
+
+
+def login_page(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f'You are now logged in as {username}.')
+                return redirect('/')
+            else:
+                messages.error(request, 'Invalid username or password')
+        else:
+            messages.error(request, 'Invalid username or password')
+    form = AuthenticationForm()
+    return render(request=request, template_name="login_page.html", context={'login_form':form})
+
+
+def logout_page(request):
+    logout(request)
+    messages.info(request, 'You have successfully logged out.')
+    return redirect('/')
